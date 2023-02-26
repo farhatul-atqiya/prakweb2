92353787f8a1e9be51c80846bcaaf3a86b2c8a8f
farhatul-atqiya <farhatulatqiya03@gmail.com>
2023-02-26 21:32:55 +0700

    update

--- array_siswa.php
+++ array_siswa.php
@@ -1,26 +1,26 @@
 <?php
 //buatlah data nilai siswa dengan array assosiatif
-$ns1 = ['id'=>1, 'nim'=>'01101','uts'=>80,'uas'=>84,'tugas'=>78];
-$ns2 = ['id'=>2, 'nim'=>'01102','uts'=>85,'uas'=>90,'tugas'=>88];
-$ns3 = ['id'=>3, 'nim'=>'01103','uts'=>89,'uas'=>95,'tugas'=>98];
+$ns1 = ['id'=>1, 'nim'=>'01101','uts'=>80,'uas'=>84,'tugas'=>78, 'nilaiAkhir'=>85];
+$ns2 = ['id'=>2, 'nim'=>'01102','uts'=>85,'uas'=>90,'tugas'=>88, 'nilaiAkhir'=>87];
+$ns3 = ['id'=>3, 'nim'=>'01103','uts'=>89,'uas'=>95,'tugas'=>98, 'nilaiAkhir'=>90];
 //simpan seluruh data variable array assosiatif 
 //ke dalam array indexing
 $ar_nilai = [$ns1,$ns2,$ns3];
 ?>
 <!--codingan html -->
-<h3 style="text-align: center;" >Daftar Nilai Siswa</h3>
+<h3 style="text-align: center;">Daftar Nilai Siswa</h3>
 <table border="1" width="100%">
-<thead>
+<thead style="background-color: pink;">
     <tr>
         <th>No</th>
         <th>NIM</th>
         <th>UTS</th>
         <th>UAS</th>
         <th>Tugas</th>
-        
+        <th>Nilai Akhir</th>
     </tr>
 </thead>
-<tbody>
+<tbody style="text-align: center;">
     <?php
     $nomor = 1;
     foreach ($ar_nilai as $nilai){
@@ -29,6 +29,7 @@ $ar_nilai = [$ns1,$ns2,$ns3];
         echo '<td>'.$nilai['uts'].'</td>';
         echo '<td>'.$nilai['uas'].'</td>';
         echo '<td>'.$nilai['tugas'].'</td>';
+        echo '<td>'.$nilai['nilaiAkhir'].'</td>';
         echo '<tr/>';
         $nomor++;
     }
