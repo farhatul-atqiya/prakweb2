c56983a9d9ee0af90627306da86f470f3d39d582
farhatul-atqiya <farhatulatqiya03@gmail.com>
2023-06-13 23:01:36 +0700

    week 12

---
+++ belanja/app/Http/Controllers/FormInputController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FormInputController extends Controller
+{
+    public function index() {
+        return view('forminput');
+    }
+
+    public function formoutput(Request $request) {
+        $data = [
+            'nama' => $request->input('nama'),
+            'email' => $request->input('email'),
+            'lokasi' => $request->input('lokasi'),
+            'jk' => $request->input('jk'),
+            'skill' => $request->input('skill'),
+        ];
+        return view ('formoutput', compact('data'));
+    }
+    
+}
